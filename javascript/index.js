c4701813c1fb5367657f2eeede8cf848487185ec
Sarita <saritasilva368@gmail.com>
2025-11-02 21:21:53 +0000

    added more cities and made dropdown fully functional

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,6 @@
+let selectedCityInterval;
+
+
 function updateClocks() {
   // London
   let londonElement = document.querySelector("#london");
@@ -6,9 +9,7 @@ function updateClocks() {
     let londonTimeElement = londonElement.querySelector(".time");
     let londonTimeZone = moment().tz("Europe/London");
     londonDateElement.innerHTML = londonTimeZone.format("MMMM Do, YYYY");
-    londonTimeElement.innerHTML = `${londonTimeZone.format(
-      "h:mm:ss"
-    )} <small>${londonTimeZone.format("A")}</small>`;
+    londonTimeElement.innerHTML = `${londonTimeZone.format("h:mm:ss")} <small>${londonTimeZone.format("A")}</small>`;
   }
 
   // New York
@@ -18,40 +19,68 @@ function updateClocks() {
     let newYorkTimeElement = newYorkElement.querySelector(".time");
     let newYorkTimeZone = moment().tz("America/New_York");
     newYorkDateElement.innerHTML = newYorkTimeZone.format("MMMM Do, YYYY");
-    newYorkTimeElement.innerHTML = `${newYorkTimeZone.format(
-      "h:mm:ss"
-    )} <small>${newYorkTimeZone.format("A")}</small>`;
+    newYorkTimeElement.innerHTML = `${newYorkTimeZone.format("h:mm:ss")} <small>${newYorkTimeZone.format("A")}</small>`;
+  }
+
+  // Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTimeZone = moment().tz("Asia/Tokyo");
+    tokyoDateElement.innerHTML = tokyoTimeZone.format("MMMM Do, YYYY");
+    tokyoTimeElement.innerHTML = `${tokyoTimeZone.format("h:mm:ss")} <small>${tokyoTimeZone.format("A")}</small>`;
+  }
+
+  // Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    let sydneyTimeZone = moment().tz("Australia/Sydney");
+    sydneyDateElement.innerHTML = sydneyTimeZone.format("MMMM Do, YYYY");
+    sydneyTimeElement.innerHTML = `${sydneyTimeZone.format("h:mm:ss")} <small>${sydneyTimeZone.format("A")}</small>`;
   }
 }
 
+
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
-  };
+
+  if (cityTimeZone === "Select a city") return;
+  if (cityTimeZone === "current") cityTimeZone = moment.tz.guess();
 
   let cityName = cityTimeZone.split("/")[1].replace("_", " ");
   let citiesElement = document.querySelector("#cities");
+
   citiesElement.innerHTML = `
-    <div class="city">
+    <div class="city" id="selected-city">
       <div>
         <h2>${cityName}</h2>
-        <div class="date">${moment()
-          .tz(cityTimeZone)
-          .format("MMMM Do, YYYY")}</div>
+        <div class="date"></div>
       </div>
-      <div class="time" id="selected-city-time">${moment()
-        .tz(cityTimeZone)
-        .format("h:mm:ss")} <small>${moment()
-    .tz(cityTimeZone)
-    .format("A")}</small></div>
+      <div class="time"></div>
     </div>
   `;
+
+
+  let cityElement = document.getElementById("selected-city");
+  let cityDateElement = cityElement.querySelector(".date");
+  let cityTimeElement = cityElement.querySelector(".time");
+
+  if (selectedCityInterval) clearInterval(selectedCityInterval);
+
+  selectedCityInterval = setInterval(() => {
+    let cityTime = moment().tz(cityTimeZone);
+    cityDateElement.innerHTML = cityTime.format("MMMM Do, YYYY");
+    cityTimeElement.innerHTML = `${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>`;
+  }, 1000);
 }
 
 updateClocks();
 setInterval(updateClocks, 1000);
 
-let citiesSelect = document.getElementById("city");
 
+let citiesSelect = document.getElementById("city");
 citiesSelect.addEventListener("change", updateCity);
+
